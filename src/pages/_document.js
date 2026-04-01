0d98346a7071ed46566d53981eb5dc456af28e22
Development-gomo <development@gomogroup.com>
2026-04-01 11:28:43 +0530

    GTM CODE UPDATE

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 import { DEFAULT_LANG } from "../../lib/api";
 
 class MyDocument extends Document {
@@ -9,6 +10,19 @@ class MyDocument extends Document {
     return (
       <Html lang={lang}>
         <Head>
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-PMXNC6T');
+  `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
@@ -18,6 +32,14 @@ class MyDocument extends Document {
         </Head>
 
         <body className="antialiased">
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PMXNC6T"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
