2e15a7a419eee7063cfedf1df65413c02641f43b
Development-gomo <development@gomogroup.com>
2026-07-23 11:17:08 +0530

    --issue fixed images

--- next.config.mjs
+++ next.config.mjs
@@ -43,8 +43,8 @@ const nextConfig = {
   },
 
   images: {
-    formats: ["image/avif", "image/webp"],
-    qualities: [55, 60, 70, 72, 75],
+    formats: ["image/webp"],
+    qualities: [55, 60, 70, 72, 75, 100],
     minimumCacheTTL: 60 * 60 * 24 * 7,
     deviceSizes: [320, 384, 640, 750, 828, 1080, 1200, 1280, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
